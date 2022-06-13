3cca8adf8270719731fb606e7af2ac183af77b95
Inácio Oliveira <inacioigne@gmail.com>
2022-06-13 13:10:18 -0400

    parser

--- import/parser.py
+++ import/parser.py
@@ -1,4 +1,9 @@
 from xml.dom.minidom import parse, parseString
 import xml.etree.ElementTree as et
 
-tree = et.parse("import/koha.xml")
+dom = parse('import/koha.xml')
+records = dom.getElementsByTagName('record')
+record = records[0]
+leader = record.getElementsByTagName('leader')[0]
+tree = et.parse('import/koha.xml')
+root = tree.getroot()
